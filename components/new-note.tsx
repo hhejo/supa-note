873a0151dd0084fa404b50c39181a1b9a67372cc
hhejo <litsmainjof@gmail.com>
2024-08-29 09:09:28 +0900

    feat: Update creating state

--- components/new-note.tsx
+++ components/new-note.tsx
@@ -2,9 +2,13 @@
 
 import { useState } from 'react';
 
-export default function NewNote() {
+export default function NewNote({ setIsCreating }) {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+
+  const onSave = async () => {
+    setIsCreating(false);
+  };
 
   return (
     <div className="w-2/3 p-2 flex flex-col gap-2 absolute top-0 bottom-0 right-0">
@@ -21,7 +25,10 @@ export default function NewNote() {
         className="border rounded-md border-gray-300 text-lg p-2 grow"
       />
       <div className="w-full flex justify-end">
-        <button className="py-1 px-3 rounded-full border-2 border-green-600 hover:bg-green-200 transition-all duration-300 ease-in-out">
+        <button
+          onClick={() => onSave()}
+          className="py-1 px-3 rounded-full border-2 border-green-600 hover:bg-green-200 transition-all duration-300 ease-in-out"
+        >
           저장
         </button>
       </div>
